559866431975c2e3778d8856167e39ff15e4c431
Lee Watson <rev@revthefox.co.uk>
2016-07-19 04:27:13 +0100

    Extract command database check to it's own method

--- lib/command.py
+++ lib/command.py
@@ -8,10 +8,13 @@ class Command(object):
         self.bot = bot
         self.config = config
         self.logger = logging.getLogger('pyper.command.' + self.name)
-        if hasattr(self, 'has_database') and self.has_database:
+        if self._has_database():
             self.database = Database('data/{0}.json'.format(self.name))
 
     def reply(self, message, reply, **kwargs):
         if 'disable_web_page_preview' not in kwargs:
             kwargs['disable_web_page_preview'] = True
         self.bot.telegram.reply_to(message, reply, **kwargs)
+
+    def _has_database(self):
+        return hasattr(self, 'has_database') and self.has_database
