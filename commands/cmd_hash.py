f9e1dd68d5f57f75da73e6d3cf4b18d815de4c2f
Lee Watson <rev@revthefox.co.uk>
2016-08-12 15:09:59 +0100

    Remove clutter from available_algorithms

--- commands/cmd_hash.py
+++ commands/cmd_hash.py
@@ -9,16 +9,15 @@ class HashCommand(Command):
 
     def run(self, message, args):
         # Remove duplicates
-        available_algorithms = list(set(x.lower() for x in hashlib.algorithms_available))
+        available_algorithms = hashlib.algorithms_guaranteed
         if not args or len(args) < 2:
             self.reply(message, '<b>/{0} [algorithm] [text]</b>, where <b>[algorithm]</b> is one of {1}'.format(
                 self.name, ', '.join(available_algorithms)), parse_mode='HTML')
             return
 
         algorithm = args[0].lower()
-        if algorithm not in [x for x in available_algorithms]:
-            self.reply(message, 'Invalid algorithm. Please choose one of {0}'.format(
-                ', '.join(available_algorithms)))
+        if algorithm not in available_algorithms:
+            self.reply(message, 'Invalid algorithm. Please choose one of {0}'.format(', '.join(available_algorithms)))
             return
 
         text = ' '.join(args[1:]).encode('utf-8')
